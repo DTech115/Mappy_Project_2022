ea86e1448e4cba04c7d3a9941ad49435fa2dbd49
DT Nesimi <ditinesimi@gmail.com>
2025-06-12 20:55:58 -0400

    fixed ceiling collision

--- Mappy Project/SpriteSheet.cpp
+++ Mappy Project/SpriteSheet.cpp
@@ -14,7 +14,7 @@ void Sprite::InitSprites(int width, int height)
 	y = -10;
 
 
-	maxFrame = 8;
+	maxFrame = 12;
 	curFrame = 0;
 	frameCount = 0;
 	frameDelay = 6;
@@ -32,41 +32,51 @@ void Sprite::UpdateSprites(int width, int height, int dir)
 	int oldx = x;
 	int oldy = y;
 
-	if(dir == 1){ //right key
-		animationDirection = 1; 
-		x+=3; 
+
+	int tileY = y / 32;
+
+	if (collided(x, y) && tileY == 0) {
+		x = oldx;
+		y = oldy + 10;
+	}
+
+	if (dir == 1) { //right key
+		animationDirection = 1;
+		x += 3;
 		if (++frameCount > frameDelay)
 		{
-			frameCount=0;
+			frameCount = 0;
 			if (++curFrame > maxFrame)
-				curFrame=1;
+				curFrame = 1;
 		}
-	} else if (dir == 0){ //left key
-		animationDirection = 0; 
-		x-=3; 
+	}
+	else if (dir == 0) { //left key
+		animationDirection = 0;
+		x -= 3;
 		if (++frameCount > frameDelay)
 		{
-			frameCount=0;
+			frameCount = 0;
 			if (++curFrame > maxFrame)
-				curFrame=1;
+				curFrame = 1;
 		}
-	}else //represent that they hit the space bar and that mean direction = 0
+	}
+	else //represent that they hit the space bar and that mean direction = 0
 		animationDirection = dir;
 
 	//check for collided with foreground tiles
-	if (animationDirection==0)
-	{ 
+	if (animationDirection == 0)
+	{
 		if (collided(x, y + frameHeight)) { //collision detection to the left
-			x = oldx; 
-			y= oldy;
+			x = oldx;
+			y = oldy;
 		}
 
 	}
-	else if (animationDirection ==1)
-	{ 
+	else if (animationDirection == 1)
+	{
 		if (collided(x + frameWidth, y + frameHeight)) { //collision detection to the right
-			x = oldx; 
-			y= oldy;
+			x = oldx;
+			y = oldy;
 		}
 	}
 }
@@ -90,7 +100,6 @@ void Sprite::DrawSprites(int xoffset, int yoffset)
 		al_draw_bitmap_region(image, fx, fy, frameWidth,frameHeight, x-xoffset, y-yoffset, ALLEGRO_FLIP_HORIZONTAL);
 	}else if (animationDirection == 2 ){
 		al_draw_bitmap_region(image,0,0,frameWidth,frameHeight,  x-xoffset, y-yoffset, 0);
-
 	}
 }
 
